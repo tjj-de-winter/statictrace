cfe85131a22a99e8fa5da837a312445f814b63b7
tjj-de-winter <140805829+tjj-de-winter@users.noreply.github.com>
2025-05-27 17:32:45 +0200

    Update appened_clones_h5ad.py

--- bin/appened_clones_h5ad.py
+++ bin/appened_clones_h5ad.py
@@ -7,12 +7,14 @@
 import scanpy as sc
 import pandas as pd
 import numpy as np
+import argparse as argp
+
 
 ### Input variables ###
 parser = argp.ArgumentParser(description = 'add clonal family information to an adata object and save as h5ad file')
 parser.add_argument('--h5ad', help = 'h5ad file without clonal information') #10X/h5ad/PSC694_v1.h5ad
 parser.add_argument('--h5ad_out', help = 'output h5ad file with clonal information') #10X/h5ad/PSC694_clonal_v1.h5ad'
-parser.add_argument('--clones_BC1', help = '.BC1.clones.csv file') # v7.linBC_matrix.BC2.clones.csv'
+parser.add_argument('--clones_BC1', help = '.BC1.clones.csv file') # v7.linBC_matrix.BC1.clones.csv'
 parser.add_argument('--clones_BC2', help = '.BC2.clones.csv file') # v7.linBC_matrix.BC2.clones.csv
 
 args = parser.parse_args()
@@ -30,9 +32,12 @@ df_bc1 = pd.read_csv(clones_BC1, names=['cellbc', 'clonal_family'], index_col=0)
 df_bc2 = pd.read_csv(clones_BC2, names=['cellbc', 'clonal_family'], index_col=0)
 
 def get_clone(cellbc, df_bc):
+	bc_dict = {cellbc: clone for cellbc, clone in zip(df_bc.index, df_bc.clonal_family)}
+
 	cellbc = cellbc.strip('-1')
-	if cellbc in df_bc.index:
-		clone = str(df_bc.loc[cellbc, 'clonal_family'])
+	if cellbc in bc_dict.values():
+		clone = bc_dict[cellbc]
+		print(clone)
 	else:
 		clone = np.nan
 	return clone
