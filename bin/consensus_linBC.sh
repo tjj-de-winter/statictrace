83aaeab8abe84cd891e02ae0dad509899ed62f4b
tjj-de-winter <140805829+tjj-de-winter@users.noreply.github.com>
2026-04-23 15:27:36 +0200

    Update consensus_linBC.sh

--- bin/consensus_linBC.sh
+++ bin/consensus_linBC.sh
@@ -3,12 +3,12 @@
 # Get true barcodes from starcode generated cluster files, only select consensus barcodes with a minimal read count 
 
 folder=$1 #starcode
-n_reads=4 # minimum read depth to call a true barcode
+n_reads=20 # minimum read depth to call a true barcode
 
 cd $folder
 
 for file in $(ls *.clustered.txt)
 do
 	outfile=${file%.clustered.txt}.true_barcodes.txt
-	awk -v threshold="$n_reads" '$2 > threshold {print $1}' "$file" > $outfile
+	awk -v threshold="$n_reads" '{if ($2 > threshold) print $1}' "$file" > $outfile
 done
